a2b88bbb069a3d69f0cea34ee1a0bd2d605aebe9
wangrn <wang_rn@163.com>
2013-01-16 13:41:22 +0800

    Update ringbuffer.h

--- ringbuffer.h
+++ ringbuffer.h
@@ -1,6 +1,8 @@
 /**
  * Project page: https://github.com/wangrn/ringbuffer
  * Copyright (c) 2013 Wang Ruining <https://github.com/wangrn>
+ * @date 2013/01/16 13:33:20
+ * @brief   a simple ringbuffer, DO NOT support dynamic expanded memory
  */
 
 #ifndef RINGBUFFER_H
