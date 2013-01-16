5b7e86fc35c6bcdfcf572ada58ce0d3f525e88de
wangrn <wang_rn@163.com>
2013-01-15 22:22:21 -0800

    Create test_ringbuffer.c

---
+++ test_ringbuffer.c
@@ -0,0 +1,7 @@
+/**
+ * Project page: https://github.com/wangrn/ringbuffer
+ * Copyright (c) 2013 Wang Ruining <https://github.com/wangrn>
+ * @date 2013/01/16 13:33:20
+ * @brief   a simple ringbuffer, DO NOT support dynamic expanded memory
+ */
+
